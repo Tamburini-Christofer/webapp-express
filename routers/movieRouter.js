fe4ab06adcc25be32a7cb6b93b292b7e4f0ea4b4
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-10-30 14:52:07 +0100

    Creazione di controller.js e movieRouter.js

---
+++ routers/movieRouter.js
@@ -0,0 +1,17 @@
+//? Framework Express
+const express = require("express");
+
+//? Controller 
+const contoller = reuire("../controllers/controller.js");
+
+//? Server
+const router = express.router();
+
+//todo Index
+router.get("/", controller.index)
+
+//todo Show
+router.get("/:id", contoller.show)
+
+//! Esportare il modulo
+export default router;
