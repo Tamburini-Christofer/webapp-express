5fc73448a9b2d35a234e571ad90e5309a6b4d0ca
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-10-30 15:04:50 +0100

    Importiamo in app.js

--- app.js
+++ app.js
@@ -7,6 +7,38 @@ const app = express();
 //? Importo una porta internet
 const port = 3000;
 
+//? Importo la rotta
+const movieRouter = require("./routers/movieRouter")
+
+//? Importo errNotFound
+const notFound = require("./middlewares/notFound");
+
+//? Importo errServer
+const errorServer = require("./middlewares/errorServer");
+
+//? Importo imagePath
+const imagePath = require("./middlewares/imagePath");
+
+//todo Middleware static 
+app.use(express.static('public'));
+
+//todo Use imagePath
+app.use(imagePath);
+
+//todo Rotta home
+app.get("/api", (req, res) => {
+    console.log("hai richiesto la home page");
+    res.send('<h1>Home page</h1>')})
+
+//todo Middleware errore server
+app.use(errorServer);
+
+//todo Middleware not found
+app.use(notFound);
+
+//todo Rotte
+app.use("/api/movie", movieRouter)
+
 //! Metto in ascolto il server dalla porta 3000
 app.listen(port, () => {
     console.log(`Il server della porta ${port} è in ascolto`)
