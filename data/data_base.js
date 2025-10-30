21b21e69be14b463742697c67e571f1a4adf7917
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-10-30 14:45:44 +0100

    Creazione di Db.js

---
+++ data/data_base.js
@@ -0,0 +1,19 @@
+//? Importo mysql2
+const mySql = require("mysql2");
+
+//? Metodo creazione oggetto
+const connection = mySql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+})
+
+//todo Avvio connessione
+connection.connect((err) => {
+    if(err) throw err;
+    console.log("Connessione con MySql!");
+});
+
+//!Esporto modulo
+export default connection;
