e8ec44fb0b6606d96c56971ea6c74f68c574766a
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-11-04 13:48:54 +0100

    Milestone 4

--- controllers/controller.js
+++ controllers/controller.js
@@ -19,33 +19,23 @@ function index(req, res) {
 
 //todo Show
 function show(req, res) {
+  const id = parseInt(req.params.id);
+  const movieSql = "SELECT * FROM movies WHERE id = ?";
+  const reviewSql = "SELECT * FROM reviews WHERE movie_id = ?";
 
-    const id = parseInt(req.params.id)
+  connection.query(movieSql, [id], (err, movieResults) => {
+    if (err) return res.status(500).json({ error: "Errore query movie" });
+    if (movieResults.length === 0) return res.status(404).json({ error: "Film non trovato" });
 
-    const movieSql = 'SELECT * FROM movies WHERE id = ?';
+    const movie = { ...movieResults[0] };
+    movie.image = req.imagePath + movie.image;
 
-    const reviewsSql = `
-    SELECT *
-    FROM reviews    
-    WHERE movie_id = ?
-    `;
-
-    connection.query(movieSql, [id], (err, movieResults) => {
-        if (err) return res.status(500).json({ error: 'Database query failed' });
-        if (movieResults.length === 0) return res.status(404).json({ error: 'Movie not found' });
-
-        const movie = movieResults[0];
-
-        movie.image = req.imagePath + movie.image
-
-        connection.query(reviewsSql, [id], (err, reviewsResults) => {
-            if (err) return res.status(500).json({ error: 'Database query failed' });
-
-            movie.reviews = reviewsResults;
-            res.json(movie);
-        });
+    connection.query(reviewSql, [id], (err, reviewResults) => {
+      if (err) return res.status(500).json({ error: "Errore query recensioni" });
+      movie.reviews = reviewResults;
+      res.json(movie);
     });
-
+  });
 }
 
 module.exports = { index, show }
