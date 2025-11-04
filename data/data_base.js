d0895dd9a99f20cce898de7bf73a46d6efea19c4
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-11-04 15:36:02 +0100

    Correzione database

--- data/data_base.js
+++ data/data_base.js
@@ -5,6 +5,7 @@ const connection = mySql.createConnection({
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "root",
   database: process.env.DB_NAME || "movie_db",
+  
 });
 
 connection.connect((err) => {
